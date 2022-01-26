78647f8cb5579c5214045a6c1f6b4cc22917d6ba
Hunter Hughes <Hunter.Hughes@vu.com>
2022-01-26 07:22:08 -0600

    New repo

--- payCalculator.py
+++ payCalculator.py
@@ -1,7 +1,23 @@
 def calculatePay():
-    
-    # This first line is provided for you
-    hrs = input("Enter Hours:")
+    # Implement your solution in between the two comment blocks
+    hrs = 0.0
+    rate = 0.0
+    pay = 0.0 #predefining variable default
+
+    try:
+        hrs = float(input("Enter Hours:"))
+        rate = float(input("Enter Hourly Rate:"))
+        print("calculating pay")
+
+        if(hrs > 40):
+            OTpay = (rate*0.5)*(hrs-40)
+        else:
+            OTpay = 0.0
+
+        pay = rate*hrs + OTpay
+
+        print(str(pay))
+    except: print("Error, please enter numeric input")
     
     # end assignment
 
@@ -9,4 +25,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+#calculatePay()
