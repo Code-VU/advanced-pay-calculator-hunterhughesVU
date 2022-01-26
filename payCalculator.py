124b2410998170f7283ed50931235f9d22629f08
Hunter Hughes <Hunter.Hughes@vu.com>
2022-01-26 07:24:40 -0600

    Added pay back to print statement

--- payCalculator.py
+++ payCalculator.py
@@ -1,13 +1,15 @@
 def calculatePay():
     # Implement your solution in between the two comment blocks
+
+    print("calculating pay")    
     hrs = 0.0
     rate = 0.0
     pay = 0.0 #predefining variable default
 
+
     try:
         hrs = float(input("Enter Hours:"))
         rate = float(input("Enter Hourly Rate:"))
-        print("calculating pay")
 
         if(hrs > 40):
             OTpay = (rate*0.5)*(hrs-40)
@@ -16,7 +18,7 @@ def calculatePay():
 
         pay = rate*hrs + OTpay
 
-        print(str(pay))
+        print("Pay: ",str(pay))
     except: print("Error, please enter numeric input")
     
     # end assignment
